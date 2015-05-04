f0c6a5aef042bf667d0db1fb658fdc7b3c764196
Ava Soleimany <asolei@mit.edu>
2015-05-03 22:52:03 -0400

    spec for feedback funciton

---
+++ javascript/feedback.js
@@ -0,0 +1,8 @@
+/* Takes information from player's submitted recipes and compares their solution to the optimal solution. */
+/* Generates and outputs feedback reflecting the player's submission. 
+/*
+/* @param submission: JavaScript object containing information from player's submitted recipes, 
+/*     where ingredients & proportions used in a given recipe are specified within another JavaScript object
+/* @param ideal: JavaScript object of the same format as submission, containing information from recipes constituting optimal solution. 
+
+
