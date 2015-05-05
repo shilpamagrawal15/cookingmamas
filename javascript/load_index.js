07c1f20d88890baa0994b8fafd32e8d22c070213
Shilpa Agrawal <shilpamagrawal15@gmail.com>
2015-05-04 22:55:32 -0400

    UI layout

--- javascript/load_index.js
+++ javascript/load_index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	$('#kitchen_cabinet').append('<div></div>').addClass('row');
 	for (i=0; i<cabinet_img_list.length; i++) {
 		var col = $('<div class="col-xs-6 col-md-3">');
-		var thumbnail = $('<a href="#" class="thumbnail" style="height:200px"><img class="cabinet-item" src="./images/'+cabinet_img_list[i]+'"></a>');
+		var thumbnail = $('<a href="#" class="thumbnail" style="height:100px"><img class="cabinet-item" src="./images/'+cabinet_img_list[i]+'"></a>');
 		col.append(thumbnail);
 		$('#kitchen_cabinet').append(col);
 	}	
