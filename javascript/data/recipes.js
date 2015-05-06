e18fc17e18f087ab4c5d17c59ef01a4972cd0b23
Shilpa Agrawal <shilpamagrawal15@gmail.com>
2015-05-05 20:55:53 -0400

    added recipes object, dynamically load static recipes

---
+++ javascript/data/recipes.js
@@ -0,0 +1,54 @@
+/*	object containing all the recipes
+	- ingredients must be in lowercase
+	- dish must be in uppercase first-letter
+	- unit must be plural or null if no unit applies
+*/
+var recipes = 
+	[
+		{
+			dish: "Peanut Butter Cookies",
+			serving: 20,
+			ingredients: [
+				{type: "peanut butter", quantity: 2, unit: "cups"},
+				{type: "white sugar", quantity: 2, unit: "cups"},
+				{type: "eggs", quantity: 2, unit: null},
+				{type: "baking soda", quantity: 2, unit: "tsp"},
+				{type: "vanilla extract", quantity: 1, unit: "tsp"},
+				{type: "butter", quantity: 4, unit: "tbsp"}
+			]
+		},
+		{
+			dish: "Lemon Pound Cake",
+			serving: 10,
+			ingredients: [
+				{type: "all-purpose flour", quantity: 1.5, unit: "cups"},
+				{type: "baking powder", quantity: 1, unit: "tbsp"},
+				{type: "butter", quantity: 8, unit: "tbsp"},
+				{type: "sugar", quantity: 1, unit: "cups"},
+				{type: "eggs", quantity: 4, unit: null},
+				{type: "vanilla extract", quantity: 2, unit: "tsp"},
+				{type: "lemon juice", quantity: 4, unit: "tbsp"}
+			]
+		},
+		{
+			dish: "Sauteed Asparagus",
+			serving: 5,
+			ingredients: [
+				{type: "thin asparagus spears", quantity: 30, unit: null},
+				{type: "lemon juice", quantity: 5, unit: "tbsp"},
+				{type: "butter", quantity: 5, unit: "tbsp"}
+			]
+		},
+		{
+			dish: "Shrimp Linguini",
+			serving: 5,
+			ingredients: [
+				{type: "linguine", quantity: 1, unit: "lbs"},
+				{type: "butter", quantity: 3, unit: "tsp"},
+				{type: "lemon juice", quantity: 1, unit: "tbsp"},
+				{type: "garlic", quantity: 2, unit: "cloves"},
+				{type: "shrimp", quantity: 8, unit: "oz"}
+			]
+		}
+
+	];
--- javascript/load_index.js
+++ javascript/load_index.js
@@ -2,11 +2,26 @@ var cabinet_img_list = ['milk_carton.png', 'flour.jpg'];
 
 $(document).ready(function () {
 	/* dynamically generate cabinet based on cabinet_img_list */
-	$('#kitchen_cabinet').append('<div></div>').addClass('row');
-	for (i=0; i<cabinet_img_list.length; i++) {
-		var col = $('<div class="col-xs-6 col-md-3">');
-		var thumbnail = $('<a href="#" class="thumbnail" style="height:100px"><img class="cabinet-item" src="./images/'+cabinet_img_list[i]+'"></a>');
-		col.append(thumbnail);
-		$('#kitchen_cabinet').append(col);
-	}	
+	
+	// $('#kitchen_cabinet').append('<div></div>').addClass('row');
+	// for (var i=0; i<cabinet_img_list.length; i++) {
+	// 	var col = $('<div class="col-xs-6 col-md-3">');
+	// 	var thumbnail = $('<a href="#" class="thumbnail" style="height:100px"><img class="cabinet-item" src="./images/'+cabinet_img_list[i]+'"></a>');
+	// 	col.append(thumbnail);
+	// 	$('#kitchen_cabinet').append(col);
+	// }
+
+	console.log("about to load recipes");
+
+	/* load the recipes onto game screen */
+	for (var i=0; i<recipes.length; i++) {
+		var static_div_name = "#recipe_" + i + "_static";
+		var static_recipe_header = $('<h4>'+ recipes[i].dish + ' <b> (Serves ' + recipes[i].serving + ') </b></h4>');
+		$(static_div_name).append(static_recipe_header);
+		var ingredients_list = recipes[i].ingredients;
+		for (var j=0; j<ingredients_list.length;j++){
+			var ingredient = recipes[i].ingredients[j].quantity + " " + recipes[i].ingredients[j].unit + " " + recipes[i].ingredients[j].type;
+			$(static_div_name).append(ingredient + "<br>");
+		}
+	}
 });
