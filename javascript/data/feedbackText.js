f26425876390991443a9b78a5718c735e31c23cc
Ava Soleimany <asolei@mit.edu>
2015-05-10 20:51:27 -0400

    beginnings of different feedback text for different levels

--- javascript/data/feedbackText.js
+++ javascript/data/feedbackText.js
@@ -1,12 +1,36 @@
-/* Object containing the feedback that should be given in gameplay scenarios.
+/* List of objects containing the feedback that should be given in gameplay scenarios for a given level. Objects are ordered by increasing levels
 	- keys are of the form boolean, boolean where the first boolean reflects proportions, the second boolean reflects level objective
 */
 
 var feedbackText = 
-	{
-		"true, true": "Way to go! You met your client's request and pleased Chef Klopfer by executing your dishes perfectly!",
-		"true, false" : "You're on your way! Chef Klopfer is pleased that you cooked your dishes using the right ingredient amounts, but you didn't meet your client's request. Keep the objective in mind when deciding what to cook!",
-		"false, true" : "Chef Klopfer noticed that your dishes taste a bit off, even though you met your client's request. Keep in mind that you have information about how each dish should be cooked!",
-		"false, false" : "Chef Klopfer noticed that your dishes taste a bit off, and your client is not pleased that you were unable to meet the request. Keep in mind that you have information about how each dish should be cooked!",
-	};
+	[
+
+		{
+			"true, true": "Way to go! You met your client's request and pleased Chef Klopfer by executing your dishes perfectly!",
+			"true, false" : "You're on your way! Chef Klopfer is pleased that you cooked your dishes using the right ingredient amounts, but you didn't meet your client's request. Keep the objective in mind when deciding what to cook!",
+			"false, true" : "Chef Klopfer noticed that your dishes taste a bit off, even though you met your client's request. Keep in mind that you have information about how each dish should be cooked!",
+			"false, false" : "Chef Klopfer noticed that your dishes taste a bit off, and your client is not pleased that you were unable to meet the request. Keep in mind that you have information about how each dish should be cooked!",
+		},
+
+		{
+			"true, true": "Way to go! You met your client's request and pleased Chef Klopfer by executing your dishes perfectly!",
+			"true, false" : "You're on your way! Chef Klopfer is pleased that you cooked your dishes using the right ingredient amounts, but you didn't meet your client's request. Keep the objective in mind when deciding what to cook!",
+			"false, true" : "Chef Klopfer noticed that your dishes taste a bit off, even though you met your client's request. Keep in mind that you have information about how each dish should be cooked!",
+			"false, false" : "Chef Klopfer noticed that your dishes taste a bit off, and your client is not pleased that you were unable to meet the request. Keep in mind that you have information about how each dish should be cooked!",
+		},
+
+		{
+			"true, true": "Way to go! You met your client's request and pleased Chef Klopfer by executing your dishes perfectly!",
+			"true, false" : "You're on your way! Chef Klopfer is pleased that you cooked your dishes using the right ingredient amounts, but you didn't meet your client's request. Keep the objective in mind when deciding what to cook!",
+			"false, true" : "Chef Klopfer noticed that your dishes taste a bit off, even though you met your client's request. Keep in mind that you have information about how each dish should be cooked!",
+			"false, false" : "Chef Klopfer noticed that your dishes taste a bit off, and your client is not pleased that you were unable to meet the request. Keep in mind that you have information about how each dish should be cooked!",
+		},
+		
+		{
+			"true, true": "Way to go! You met your client's request and pleased Chef Klopfer by executing your dishes perfectly!",
+			"true, false" : "You're on your way! Chef Klopfer is pleased that you cooked your dishes using the right ingredient amounts, but you didn't meet your client's request. Keep the objective in mind when deciding what to cook!",
+			"false, true" : "Chef Klopfer noticed that your dishes taste a bit off, even though you met your client's request. Keep in mind that you have information about how each dish should be cooked!",
+			"false, false" : "Chef Klopfer noticed that your dishes taste a bit off, and your client is not pleased that you were unable to meet the request. Keep in mind that you have information about how each dish should be cooked!",
+		},
+	]
 
